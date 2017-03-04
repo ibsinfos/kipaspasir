fda30fb09090e285f68060d95c2b0786d1d3c8d6
dayangkartini <dayangkartiniharon@gmail.com>
2017-03-04 12:30:55 +0800

    4/3 connected linked and head adjustment

--- wordpress/wp-content/themes/twentyseventeen/footer.php
+++ wordpress/wp-content/themes/twentyseventeen/footer.php
@@ -38,11 +38,34 @@
 				get_template_part( 'template-parts/footer/site', 'info' );
 				?>
 				<!---hahahahaha-->
-				
+				<div id="footer-widgets">
+
+<div id="sidebar-1">
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer 1') ) : ?>
+<?php endif; ?>
+</div>
+
+<div id="sidebar-2">
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer 2') ) : ?>
+<?php endif; ?>
+</div>
+
+<div id="sidebar-3">
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer 3') ) : ?>
+<?php endif; ?>
+</div>
+
+<div id="sidebar-4">
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer 4') ) : ?>
+<?php endif; ?>
+</div>
+</div>
+<div style="clear-both"></div>
 			</div><!-- .wrap -->
 			<div class=bottombar1>Copyright © 2017 Alladein. DinarPal Group. All Rights Reserved. </div>
 		</footer><!-- #colophon -->
 	</div><!-- .site-content-contain -->
+	
 	
 </div><!-- #page -->
 
