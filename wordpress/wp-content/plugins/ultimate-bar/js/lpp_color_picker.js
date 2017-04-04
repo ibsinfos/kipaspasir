b5d1a4cdac039fe4ed6ef55d146a03a437caae66
dayangkartini <dayangkartiniharon@gmail.com>
2017-04-04 18:09:40 +0800

    testing problem

--- wordpress/wp-content/plugins/ultimate-bar/js/lpp_color_picker.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function(){
-			    jQuery('.ub_color_picker').wpColorPicker();
-
-			});
--- wordpress/wp-content/plugins/woo-paypal-advanced/admin/js/pal-advanced-admin.js
+++
@@ -1,3 +0,0 @@
-jQuery(function($) {    
-    //jQuery('.premium_paypal_advanced_color').wpColorPicker();
-});
