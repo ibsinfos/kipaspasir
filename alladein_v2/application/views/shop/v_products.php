55389eaeee98db82d55f16b4af33df07041fa4c2
dayangkartini <dayangkartiniharon@gmail.com>
2017-04-20 18:07:35 +0800

    list

--- alladein_v2/application/views/shop/v_products.php
+++ alladein_v2/application/views/shop/v_products.php
@@ -1,102 +1,15 @@
  <div id="wrapper" class="container">    
 			<section class="header_text sub">
 			<img class="pageBanner" src="<?=base_url(); ?>assets/themes/images/pageBanner.png" alt="New products" >
-				<h4><span>New products</span></h4>
+				<h4><span>Product List</span></h4>
 			</section>
 			<section class="main-content">
+			
 				
-				<div class="row">						
-					<div class="span9">								
-						<ul class="thumbnails listing-products">
-							<li class="span3">
-								<div class="product-box">
-									<span class="sale_tag"></span>												
-									<a href="product_detail.html"><img alt="" src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg"></a><br/>
-														<a href="product_detail.html" class="title">Aniza Coklat Sedap</a><br/>
-														<a href="#" class="category">30% less for 5 first customers</a>
-														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
-								</div>
-							</li>       
-							<li class="span3">
-								<div class="product-box">												
-									<a href="product_detail.html"><img alt="" src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg"></a><br/>
-														<a href="product_detail.html" class="title">Aniza Coklat Sedap</a><br/>
-														<a href="#" class="category">30% less for 5 first customers</a>
-														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
-								</div>
-							</li>
-							<li class="span3">
-								<div class="product-box">
-									<span class="sale_tag"></span>												
-									<a href="product_detail.html"><img alt="" src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg"></a><br/>
-														<a href="product_detail.html" class="title">Aniza Coklat Sedap</a><br/>
-														<a href="#" class="category">30% less for 5 first customers</a>
-														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
-								</div>
-							</li>
-							<li class="span3">
-								<div class="product-box">												
-									<span class="sale_tag"></span>
-									<a href="product_detail.html"><img alt="" src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg"></a><br/>
-														<a href="product_detail.html" class="title">Aniza Coklat Sedap</a><br/>
-														<a href="#" class="category">30% less for 5 first customers</a>
-														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
-								</div>
-							</li>
-							<li class="span3">
-								<div class="product-box">                                        												
-									<a href="product_detail.html"><img alt="" src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg"></a><br/>
-														<a href="product_detail.html" class="title">Aniza Coklat Sedap</a><br/>
-														<a href="#" class="category">30% less for 5 first customers</a>
-														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
-								</div>
-							</li>       
-							<li class="span3">
-								<div class="product-box">												
-									<a href="product_detail.html"><img alt="" src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg"></a><br/>
-														<a href="product_detail.html" class="title">Aniza Coklat Sedap</a><br/>
-														<a href="#" class="category">30% less for 5 first customers</a>
-														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
-								</div>
-							</li>
-							<li class="span3">
-								<div class="product-box">												
-									<a href="product_detail.html"><img alt="" src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg"></a><br/>
-														<a href="product_detail.html" class="title">Aniza Coklat Sedap</a><br/>
-														<a href="#" class="category">30% less for 5 first customers</a>
-														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
-								</div>
-							</li>
-							<li class="span3">
-								<div class="product-box">												
-									<a href="product_detail.html"><img alt="" src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg"></a><br/>
-														<a href="product_detail.html" class="title">Aniza Coklat Sedap</a><br/>
-														<a href="#" class="category">30% less for 5 first customers</a>
-														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
-								</div>
-							</li>
-							<li class="span3">
-								<div class="product-box">												
-									<a href="product_detail.html"><img alt="" src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg"></a><br/>
-														<a href="product_detail.html" class="title">Aniza Coklat Sedap</a><br/>
-														<a href="#" class="category">30% less for 5 first customers</a>
-														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
-								</div>
-							</li>
-						</ul>								
-						<hr>
-						<div class="pagination pagination-small pagination-centered">
-							<ul>
-								<li><a href="#">Prev</a></li>
-								<li class="active"><a href="#">1</a></li>
-								<li><a href="#">2</a></li>
-								<li><a href="#">3</a></li>
-								<li><a href="#">4</a></li>
-								<li><a href="#">Next</a></li>
-							</ul>
-						</div>
-					</div>
-					<div class="span3 col">
+					
+					
+			<div class="row">	
+			<div class="span3 col">
 						<div class="block">	
 							<ul class="nav nav-list">
 								<li class="nav-header">TOP CATEGORIES</li>
@@ -134,7 +47,7 @@
 												<div class="product-box">
 													<span class="sale_tag"></span>												
 													<a href="product_detail.html"><img alt="" src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg"></a><br/>
-														<a href="product_detail.html" class="title">Aniza Coklat Sedap</a><br/>
+														<a href="product_detail.html" class="title">PERFUME AURA</a><br/>
 														<a href="#" class="category">30% less for 5 first customers</a>
 														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
 												</div>
@@ -146,7 +59,7 @@
 											<li class="span3">
 												<div class="product-box">												
 													<a href="product_detail.html"><img alt="" src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg"></a><br/>
-														<a href="product_detail.html" class="title">Aniza Coklat Sedap</a><br/>
+														<a href="product_detail.html" class="title">PERFUME AURA</a><br/>
 														<a href="#" class="category">30% less for 5 first customers</a>
 														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
 												</div>
@@ -163,23 +76,127 @@
 									<a href="#" title="Praesent tempor sem sodales">
 										<img src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg" alt="Praesent tempor sem sodales">
 									</a>
-									<a href="#">Aniza Coklat Sedap</a>
+									<a href="#">PERFUME AURA</a>
 								</li>
 								<li>
 									<a href="#" title="Luctus quam ultrices rutrum">
 										<img src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg" alt="Luctus quam ultrices rutrum">
 									</a>
-									<a href="#">Aniza Coklat Sedap</a>
+									<a href="#">PERFUME AURA</a>
 								</li>
 								<li>
 									<a href="#" title="Fusce id molestie massa">
 										<img src="<?=base_url(); ?>assets/themes/images/ladies/10.jpg" alt="Fusce id molestie massa">
 									</a>
-									<a href="#">Aniza Coklat Sedap</a>
+									<a href="#">PERFUME AURA</a>
 								</li>   
 							</ul>
 						</div>
 					</div>
-				</div>
+					<div class="span9" style="
+    margin-top: -13px;
+">		
+					<h4 class="title" style="    width: 100%!important;">
+                                    <span class="pull-left"><span class="text"><span class="line">Latest<strong> Products</strong></span></span></span>
+                                    <span class="pull-right">
+                                        <a class="left button" href="#myCarousel" data-slide="prev"></a><a class="right button" href="#myCarousel" data-slide="next"></a>
+                                    </span>
+                                </h4>
+						<ul class="thumbnails listing-products">
+						
+							
+							
+							
+							<li class="span3">
+								<div class="product-box">												
+									<a href="<?=site_url('shop/showProductDetail'); ?>"><iframe width="250" height="188" src="https://www.youtube.com/embed/MTGLXU0-BtI" frameborder="0" allowfullscreen></iframe></a><br/>
+														<a href="product_detail.html" class="title">DRPERFUME CAR PERFUME</a><br/>
+														<a href="#" class="category">30% less for 5 first customers</a>
+														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
+								</div>
+							</li>
+							<li class="span3">
+								<div class="product-box">												
+								<a href="<?=site_url('shop/showProductDetail'); ?>"><iframe width="250" height="188" src="https://www.youtube.com/embed/9hIuHe2nXo4" frameborder="0" allowfullscreen></iframe></a></a><br/>
+														<a href="product_detail.html" class="title">SAMBAL PERFUME</a><br/>
+														<a href="#" class="category">30% less for 5 first customers</a>
+														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
+								</div>
+							</li>
+							<li class="span3">
+								<div class="product-box">												
+									<a href="<?=site_url('shop/showProductDetail'); ?>"><iframe width="250" height="188" src="https://www.youtube.com/embed/NcjABBAlszo" frameborder="0" allowfullscreen></iframe>
+														<a href="product_detail.html" class="title">PANTENE SHAMPOO</a><br/>
+														<a href="#" class="category">30% less for 5 first customers</a>
+														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
+								</div>
+							</li>
+						
+							<li class="span3">
+								<div class="product-box">												
+									<a href="<?=site_url('shop/showProductDetail'); ?>"><iframe width="250" height="188" src="https://www.youtube.com/embed/MTGLXU0-BtI" frameborder="0" allowfullscreen></iframe></a><br/>
+														<a href="product_detail.html" class="title">DRPERFUME CAR PERFUME</a><br/>
+														<a href="#" class="category">30% less for 5 first customers</a>
+														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
+								</div>
+							</li>
+							<li class="span3">
+								<div class="product-box">												
+								<a href="<?=site_url('shop/showProductDetail'); ?>"><iframe width="250" height="188" src="https://www.youtube.com/embed/9hIuHe2nXo4" frameborder="0" allowfullscreen></iframe></a></a><br/>
+														<a href="product_detail.html" class="title">SAMBAL PERFUME</a><br/>
+														<a href="#" class="category">30% less for 5 first customers</a>
+														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
+								</div>
+							</li>
+							<li class="span3">
+								<div class="product-box">												
+									<a href="<?=site_url('shop/showProductDetail'); ?>"><iframe width="250" height="188" src="https://www.youtube.com/embed/NcjABBAlszo" frameborder="0" allowfullscreen></iframe>
+														<a href="product_detail.html" class="title">PANTENE SHAMPOO</a><br/>
+														<a href="#" class="category">30% less for 5 first customers</a>
+														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
+								</div>
+							</li>
+							<li class="span3">
+								<div class="product-box">												
+									<a href="<?=site_url('shop/showProductDetail'); ?>"><iframe width="250" height="188" src="https://www.youtube.com/embed/MTGLXU0-BtI" frameborder="0" allowfullscreen></iframe></a><br/>
+														<a href="product_detail.html" class="title">DRPERFUME CAR PERFUME</a><br/>
+														<a href="#" class="category">30% less for 5 first customers</a>
+														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
+								</div>
+							</li>
+							<li class="span3">
+								<div class="product-box">												
+								<a href="<?=site_url('shop/showProductDetail'); ?>"><iframe width="250" height="188" src="https://www.youtube.com/embed/9hIuHe2nXo4" frameborder="0" allowfullscreen></iframe></a></a><br/>
+														<a href="product_detail.html" class="title">SAMBAL PERFUME</a><br/>
+														<a href="#" class="category">30% less for 5 first customers</a>
+														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
+								</div>
+							</li>
+							<li class="span3">
+								<div class="product-box">												
+									<a href="<?=site_url('shop/showProductDetail'); ?>"><iframe width="250" height="188" src="https://www.youtube.com/embed/NcjABBAlszo" frameborder="0" allowfullscreen></iframe>
+														<a href="product_detail.html" class="title">PANTENE SHAMPOO</a><br/>
+														<a href="#" class="category">30% less for 5 first customers</a>
+														<p class="price">3.0 Gram DPS <br> 0.2 Gram DPG</p>
+								</div>
+							</li>
+						</ul>								
+						<hr>
+						<div class="pagination pagination-small pagination-centered">
+							<ul>
+								<li><a href="#">Prev</a></li>
+								<li class="active"><a href="#">1</a></li>
+								<li><a href="#">2</a></li>
+								<li><a href="#">3</a></li>
+								<li><a href="#">4</a></li>
+								<li><a href="#">Next</a></li>
+							</ul>
+							
+						
+							
+							
+						</div>
+					</div>
+</div>
 			</section>
 </div>			
